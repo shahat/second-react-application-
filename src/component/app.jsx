d1e8471f5fe5df6ec18ba038f57600c3c6979df6
shahat <me6847570@gmail.com>
2020-08-16 10:25:10 +0200

    commited in 16/8/2020

---
+++ src/component/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import Shopingcart from "./shoppingcard";
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return (
+            <Fragment>
+                <Shopingcart />
+            </Fragment>
+          );
+    }
+}
+ 
+export default App;
---
+++ src/component/shoppingcard.jsx
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+class Shopingcart extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <Fragment>
+            <h1> this is shoping cart </h1>
+            </Fragment>
+        );
+    }
+}
+ 
+export default Shopingcart;
